b6c4eb57cde72ce9de02db0869b8f5d41108416f
Simon Bogina <simon@bogina.si>
2025-11-21 12:45:50 +0100

    Exercises 1-1 and 1-2

---
+++ src/chapter1/1-2.c
@@ -0,0 +1,11 @@
+/* Experiment to find out what happens when printf's argument string contains
+\c, where c is some character not listed above.*/
+
+#include <stdio.h>
+int main()
+{
+    printf("hello, world!\g");
+    return 0;
+}
+
+/* compiler: unknown escape sequence: '\g' */
