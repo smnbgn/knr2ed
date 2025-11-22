d14215d016e1243aec77056d961ff4a158465517
Simon Bogina <simon@bogina.si>
2025-11-22 13:05:04 +0100

    Add exercise 1-6

---
+++ src/chapter1/1-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Verify that the expression getchar() != EOF is 0 or 1 */
+
+int main()
+{
+    int c;
+
+    while (c = getchar() != EOF)
+        printf("%d\n", c);
+
+    /* EOF reached */
+    printf("%d\n", c);
+
+    return 0;
+}
+
+
