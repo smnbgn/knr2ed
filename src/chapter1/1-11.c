ff818005254fa42ff2fb3ef4464a302249153457
Simon Bogina <simon@bogina.si>
2025-11-24 10:05:05 +0100

    Add exercise 1-11

---
+++ src/chapter1/1-11.c
@@ -0,0 +1,6 @@
+/* How would you test the word count program? What kinds of input are most
+likely to uncover bugs if there are any? */
+
+/* boundary conditions: no input, characters + words, newlines + characters*,
+blank + word at the beginning of a line */
+
