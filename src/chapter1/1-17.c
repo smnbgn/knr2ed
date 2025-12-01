44d2ccec0024a8fa004fb45f63e5082d4fa96bbc
Simon Bogina <simon@bogina.si>
2025-12-01 13:51:37 +0100

    Add exercise 1-17

---
+++ src/chapter1/1-17.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size */
+#define LIMIT 80
+
+int getline(char line[], int maxline);
+
+/* Write a program to print all input lines that are longer than 80 
+characters. */
+int main()
+{
+    int len;
+    char line[MAXLINE];
+
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > LIMIT) {
+            printf("%s", line);
+        }
+    }
+    
+    return 0;
+}
+
+int getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i<lim-1 && (c=getchar())!= EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
