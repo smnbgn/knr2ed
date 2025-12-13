ff1ab784da55f39558aa729b89cf7cd592e945c5
Simon Bogina <simon@bogina.si>
2025-12-13 19:10:33 +0100

    Add exercise instruction

--- src/chapter1/1-20.c
+++ src/chapter1/1-20.c
@@ -6,6 +6,9 @@
 int getline(char s[], int lim);
 void detab (char output[], char input[]);
 
+/* Write a program detab that replaces tabs in the input with the proper number
+of blanks to space to the next tab stop. Assume a fixed set of tab stops, say
+every n columns. Should n be a variable or a symbolic parameter? */
 int main()
 {
     char line[MAXLINE];
