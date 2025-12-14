f394af9760bbcf42df301b63829d7be2203f1a2f
Simon Bogina <simon@bogina.si>
2025-12-14 05:10:21 +0100

    Add exercise 1-20 (without arrays)

--- src/chapter1/1-20.c
+++ src/chapter1/1-20.c
@@ -1,54 +1,27 @@
 #include <stdio.h>
 
 #define TABSTOP 8
-#define MAXLINE 1000
 
-int getline(char s[], int lim);
-void detab (char output[], char input[]);
-
-/* Write a program detab that replaces tabs in the input with the proper number
-of blanks to space to the next tab stop. Assume a fixed set of tab stops, say
-every n columns. Should n be a variable or a symbolic parameter? */
 int main()
 {
-    char line[MAXLINE];
-    char detab_line[MAXLINE * TABSTOP];
+    int c;
+    int column, blanks, i;
  
-    while(getline(line, MAXLINE) > 0) {
-        detab(detab_line, line);
-        printf("%s", detab_line);
-    }
-    return 0;
-}
-
-int getline(char s[], int lim) 
-{
-    int c, i;
-
-    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-        s[i] = c;
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
-}
-
-void detab(char output[], char input[]) 
-{
-    int i, j, k, spaces;
-
-    for (i=0, j=0; input[i] != '\0'; ++i) {
-        if (input[i] == '\t') {
-            
-            spaces = TABSTOP - (j % TABSTOP);
-            for(k=0; k < spaces; ++k) {
-                output[j++] = ' ';
+    column = 1;
+    while((c=getchar()) != EOF) {
+        if (c == '\n') {
+            putchar(c);
+            column = 1;
+        } else if (c == '\t'){
+            blanks = TABSTOP - ((column - 1) % TABSTOP);
+            for(i=0; i < blanks; i++) {
+                putchar(' ');
+                column++;
             }
         } else {
-            output[j++] = input[i];
+            putchar(c);
+            column++;
         }
     }
-    output[j] = '\0';
+    return 0;
 }
