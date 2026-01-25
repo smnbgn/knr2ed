742a96a74b228461f8a098e21f72b944d30c6c33
Simon Bogina <simon@bogina.si>
2026-01-25 17:12:51 +0100

    Add exercise 2-1

---
+++ src/chapter2/2-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    printf("signed char min: %d\n", SCHAR_MIN);
+    printf("signed char max: %d\n", SCHAR_MAX);
+    printf("unsigned char max: %u\n", UCHAR_MAX);
+
+    printf("shrt min: %d\n", SHRT_MIN);
+    printf("shrt max: %d\n", SHRT_MAX);
+    printf("unsigned shrt max: %u\n", USHRT_MAX);
+
+    printf("int min: %d\n", INT_MIN);
+    printf("int max: %d\n", INT_MAX);
+    printf("unsigned int max: %u\n", UINT_MAX);
+
+    printf("long min: %ld\n", LONG_MIN);
+    printf("long max: %ld\n", LONG_MAX);
+    printf("unsigned long max: %u\n", ULONG_MAX);
+
+    printf("long long max: %lld\n", LLONG_MAX);
+    printf("long long min: %lld\n", LLONG_MIN);
+    printf("unsigned long long max: %llu\n", ULLONG_MAX);
+}
